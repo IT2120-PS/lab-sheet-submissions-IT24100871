7f539f4a9b50b7e95b493f656096dbf700a5ddff
IT24100871 <it24100871@my.sliit.lk>
2025-10-16 22:41:38 +0530

    Add files via upload
    IT24100871_Lab_10

--- Lab Sheet 10/IT24100871_PS_Lab_10.R
+++ Lab Sheet 10/IT24100871_PS_Lab_10.R
@@ -1,4 +1,17 @@
 #1
+#i
+#null hypotheses - Probability that customer's choice of four snack types (A, B,
+#C, D) with equal probability of 0.25
+#alternative hypotheses - At least one snack type exists such that probability of
+#customer's choice will be different from 0.25
+
+#2
 observed <- c(120,95,85,100)
 prob <- c(.25,.25,.25,.25)
 chisq.test(x=observed,p=prob)
+
+#3
+#p value(0.08966) is greater than 0.05
+#So that, do not reject null hypotheses at 5%
+#Therefore we can conclude that Probability that customer's choice of four snack types (A, B,
+#C, D) will be same which is 0.25.
